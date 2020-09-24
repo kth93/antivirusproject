c033a0834984afe8227f317e3fb923bffe6d026d
nDJay11 <ndjay11@github.com>
2020-09-24 22:51:36 +0900

    modified kmake.py to enable decryption

--- kmake.py
+++ kmake.py
@@ -2,16 +2,46 @@ import sys
 import zlib
 import hashlib
 import os
+from argparse import ArgumentParser
 
-def main() :
-    if len(sys.argv) != 2:
-        print("Usage : kmake.py [file]")
+def DecodeKMD(fname):
+    if fname.split('.')[-1] != 'kmd':
+        print("input file is not vaild")
         return
+    fp = open(fname, 'rb')
+    buf = fp.read()
+    fp.close()
 
-    fname = sys.argv[1]
-    tname = fname
+    buf2 = buf[:-32]
+    fmd5 = buf[-32:]
 
-    fp = open(tname, 'rb')
+    f = buf2
+    for i in range(3):
+        md5 = hashlib.md5()
+        md5.update(f)
+        f = md5.hexdigest()
+
+    if f != fmd5:
+        raise SystemError
+
+    buf3 = ''
+    for c in buf[4:]:
+        buf3 += chr(ord(c) ^ 0xFF)
+
+    buf4 = zlib.decompress(buf3)
+
+    db_name = fname.split('.')[-2] + '.db'
+
+    fp = open(db_name, 'wb')
+    fp.write(buf4)
+    fp.close()
+    print("{} -> {}".format(fname, db_name))
+
+def EncryptKMD(fname) :
+    if fname.split('.')[-1] != 'db':
+        print("input file is not vaild")
+        return
+    fp = open(fname, 'rb')
     buf = fp.read()
     fp.close()
 
@@ -38,5 +68,19 @@ def main() :
 
     print("{} -> {}".format(fname, kmd_name))
 
+def main():
+    parser = ArgumentParser(description='db encrypt & decrypt tool')
+    parser.add_argument('filenames', nargs='?', help='Files to encrypt or decrypt')
+    parser.add_argument('--decrypt', '-d', action='store_true', help='decrpyt .kmd file to .db file')
+    args = parser.parse_args()
+
+    fname = args.filenames
+    fname = fname.replace('.\\', '')
+    if args.decrypt:
+        DecodeKMD(fname)
+    else:
+        EncryptKMD(fname)
+
 if __name__ == "__main__":
     main()
+    
