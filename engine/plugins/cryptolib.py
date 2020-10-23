38dbbed26c86939c4f62d248778f2ae8e5fe34d1
nDJay11 <ndjay11@github.com>
2020-10-23 23:46:26 +0900

    cryptolib made into a library

--- engine/plugins/cryptolib.py
+++ engine/plugins/cryptolib.py
@@ -2,3 +2,20 @@ import hashlib
 
 def md5(data):
     return hashlib.md5(data).hexdigest()
+
+class KavMain:
+    def init(self, plugins_path):
+        return 0
+    
+    def uninit(self):
+        return 0
+
+    def getinfo(self):
+        info = dict()
+
+        info['author'] = 'kth93'
+        info['version'] = '1.0'
+        info['title'] = 'Crypto Library'
+        info['kmd_name'] = 'cryptolib'
+
+        return info
